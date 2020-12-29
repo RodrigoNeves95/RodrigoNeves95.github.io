a2919b20a03b3f77fcc46ab852469ef760b9a0ad
RodrigoNeves95 <rodrigo.f.l.neves@gmail.com>
2020-12-29 13:58:14 +0000

    added new website code

---
+++ website/content/jobs/Jungle/index.md
@@ -0,0 +1,13 @@
+---
+date: '2020-12-22'
+title: 'Machine Learning Engineer'
+company: 'Jungle'
+location: 'Lisbon, Portugal'
+range: 'May 2018 -  Present'
+url: 'https://www.jungle.ai/'
+---
+
+- Research and implementation of predictive maintenance solutions
+- Development and deployment models to Jungle product, using mostly python, pytorch and kubernetes
+- Implementation of KPI and bussiness inteliggence metrics for Jungle product
+- Integrated team that planned and implemented jungle infrastructure and production environment
---
+++ website/src/components/icons/logo.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const IconLogo = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="50.613"
+      height="37.833"
+      fillRule="evenodd"
+      strokeLinejoin="round"
+      preserveAspectRatio="xMidYMid"
+      viewBox="1100 1500 1101 1001"
+    >
+      <defs>
+        <font horizAdvX="2048">
+          <font-face
+            ascent="1905"
+            descent="476"
+            fontFamily="DejaVu Serif embedded"
+            fontStyle="normal"
+            fontWeight="bold"
+            unitsPerEm="2048"
+          ></font-face>
+        </font>
+      </defs>
+      <g>
+        <text>
+          <tspan
+            fontFamily="DejaVu Serif, serif"
+            fontSize="635"
+            fontWeight="700"
+            fill="currentColor"
+          >
+            <tspan x="1086" y="2222">
+              <tspan>RN</tspan>
+            </tspan>
+          </tspan>
+        </text>
+      </g>
+    </svg>
+  );
+}
+
+export default IconLogo;
